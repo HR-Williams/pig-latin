031f4ec45817c6e21345c7115338d2e96a1be203
bo zhao and Halle Williams <halle@>
2016-06-08 11:59:19 -0700

    move all consonants to the end and add ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 // business logic
 var transfer = function(word){
-  if(word.charAt(0)==="a"||word.charAt(0)==="e"||word.charAt(0)==="i"||word.charAt(0)==="o"||word.charAt(0)==="u"||word.charAt(0)==="A"||word.charAt(0)==="E"||word.charAt(0)==="I"||word.charAt(0)==="O"||word.charAt(0)==="U"){
-    return word + "ay";
-  }
-  else{
-    word = word.split("");
-    word.push(word.shift());
-    return word.join("") + "ay";
+  // if(word.charAt(0)==="a"||word.charAt(0)==="e"||word.charAt(0)==="i"||word.charAt(0)==="o"||word.charAt(0)==="u"||word.charAt(0)==="A"||word.charAt(0)==="E"||word.charAt(0)==="I"||word.charAt(0)==="O"||word.charAt(0)==="U"){
+  //   return word + "ay";
+  // }
+  //
+  // else{
+  //   word = word.split("");
+  //   word.push(word.shift());
+  //   return word.join("") + "ay";
+  // }
+
+  var result = word;
+  for(var i=0; i<word.length; i++){
+    if(word.charAt(i)==="a"||word.charAt(i)==="e"||word.charAt(i)==="i"||word.charAt(i)==="o"||word.charAt(i)==="u"||word.charAt(i)==="A"||word.charAt(i)==="E"||word.charAt(i)==="I"||word.charAt(i)==="O"||word.charAt(i)==="U"){
+      return result + "ay";
+    }
+    else{
+      result = result.split("");
+      result.push(result.shift());
+      result = result.join("");
+    }
   }
 }
 
