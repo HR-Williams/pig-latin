f42d9c90d49221ae8cfe663bee0d100ba2b416d3
bo zhao and Halle Williams <bo@>
2016-06-08 13:42:21 -0700

    treat qu as a consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 // business logic
 var transfer = function(word){
-  // if(word.charAt(0)==="a"||word.charAt(0)==="e"||word.charAt(0)==="i"||word.charAt(0)==="o"||word.charAt(0)==="u"||word.charAt(0)==="A"||word.charAt(0)==="E"||word.charAt(0)==="I"||word.charAt(0)==="O"||word.charAt(0)==="U"){
-  //   return word + "ay";
-  // }
-  //
-  // else{
-  //   word = word.split("");
-  //   word.push(word.shift());
-  //   return word.join("") + "ay";
-  // }
 
-  var result = word;
+  var result = word.toLowerCase();
   for(var i=0; i<word.length; i++){
-    if(word.charAt(i)==="a"||word.charAt(i)==="e"||word.charAt(i)==="i"||word.charAt(i)==="o"||word.charAt(i)==="u"||word.charAt(i)==="A"||word.charAt(i)==="E"||word.charAt(i)==="I"||word.charAt(i)==="O"||word.charAt(i)==="U"){
-      return result + "ay";
+    if(word.charAt(0)==="q" && word.charAt(1)==="u"){
+      return word.slice(2) + word.slice(0,2) + "ay";
+    }
+    else if(result.charAt(i)==="a" || result.charAt(i)==="e" || result.charAt(i)==="i" || result.charAt(i)==="o" || result.charAt(i)==="u"){
+      return word + "ay";
     }
     else{
-      result = result.split("");
-      result.push(result.shift());
-      result = result.join("");
+      word = word.split("");
+      word.push(word.shift());
+      word = word.join("");
     }
   }
 }
